3234e4afcf12e8df8544c5b8e3ff96c280e0b9cd
José Luis Chiquete Valdivieso <josech@gmail.com>
2021-11-08 14:15:03 -0600

    Corrección de validación de promedio.

--- schemas.py
+++ schemas.py
@@ -10,7 +10,7 @@ class AlumnoSchema(Schema):
     segundo_apellido = String(required=False, validate=Length(min=2, max=50))
     carrera = String(required=True, validate=OneOf(carreras))
     semestre = Integer(required=True, validate=Range(min=1, max=50))
-    promedio = Float(required=True, validate=Range(min=1, max=50))
+    promedio = Float(required=True, validate=Range(min=0, max=10))
     al_corriente = Boolean(required=True)
 
 class AlumnoInSchema(Schema):
@@ -19,5 +19,5 @@ class AlumnoInSchema(Schema):
     segundo_apellido = String(required=False, validate=Length(min=2, max=50))
     carrera = String(required=True, validate=OneOf(carreras))
     semestre = Integer(required=True, validate=Range(min=1, max=50))
-    promedio = Float(required=True, validate=Range(min=1, max=50))
+    promedio = Float(required=True, validate=Range(min=0, max=10))
     al_corriente = Boolean(required=True)
